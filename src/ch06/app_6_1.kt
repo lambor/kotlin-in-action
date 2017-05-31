036f3b7f7b4e9016f660dd43cb1db579cc9a5f7d
Lambor <1973124271@qq.com>
2017-05-31 18:12:55 +0800

    temp commit

--- src/ch06/app_6_1.kt
+++ src/ch06/app_6_1.kt
@@ -1,5 +1,6 @@
 package ch06
 
+
 /**
  * Created by lambor on 17-5-26.
  * Nullability
@@ -17,6 +18,18 @@ class Employee(val name:String,val manager:Employee?)
 fun managerName(employee:Employee):String? = employee.manager?.name
 
 
+fun foo(s:String?) = s?:"";
+
+fun foo2(s:String?) = s?:throw NullPointerException()
+
+class Person(val firstName:String,val lastName:String) {
+    override fun equals(other: Any?): Boolean {
+        return (other as? Person ?:return false).equal(this)
+    }
+
+    fun equal(person:Person) = person.firstName == firstName && person.lastName == lastName
+}
+
 fun main(args: Array<String>) {
     printAllCaps("abc")
     printAllCaps(null)
@@ -25,4 +38,13 @@ fun main(args: Array<String>) {
     val developer = Employee("Bob Smith",ceo)
     println(managerName(developer))
     println(managerName(ceo))
+
+    foo(null)
+    foo("123")
+//    foo2(null)
+    foo2("123")
+
+    val person1:Person? = Person("1","2")
+    val person2:Person? = null
+    println(person1 == person2)
 }
